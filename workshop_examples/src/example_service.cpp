fa0143409f4a23b96be2483b888a860490ed26f7
but-spanel <spanel@fit.vutbr.cz>
2013-10-11 10:32:01 +0200

    Service definition added

--- workshop_examples/src/example_service.cpp
+++ workshop_examples/src/example_service.cpp
@@ -6,29 +6,29 @@
 
 #include <ros/ros.h>
 
-#include <workshop_msgs/Detections.h>
+#include <workshop_msgs/DetectionsStamped.h>
 
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "publisher");
 
     ros::NodeHandle n;
-    ros::Publisher pub = n.advertise<workshop_msgs::Detections>("face_detections_out", 1000);
+    ros::Publisher pub = n.advertise<workshop_msgs::DetectionsStamped>("face_detections_out", 1000);
 
     ros::Rate loop_rate(10);
     while( ros::ok() )
     {
-        workshop_msgs::Detections msg;
+        workshop_msgs::DetectionsStamped msg;
         msg.header.stamp = ros::Time::now();
         msg.header.frame_id = "";
-        msg.type = workshop_msgs::Detections::FACE;
+        msg.detections.type = workshop_msgs::Detections::FACE;
 
         sensor_msgs::RegionOfInterest det;
         det.x_offset = 100;
         det.y_offset = 100;
         det.width = 64;
         det.height = 64;
-        msg.detections.push_back(det);
+        msg.detections.rects.push_back(det);
 
         ROS_INFO_ONCE( "Message sent" );
 
