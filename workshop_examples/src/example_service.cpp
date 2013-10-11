9d34c8096426dd1ff64b47699827cf1481b8d07b
but-spanel <spanel@fit.vutbr.cz>
2013-10-11 10:48:47 +0200

    Service sample added

--- workshop_examples/src/example_service.cpp
+++ workshop_examples/src/example_service.cpp
@@ -6,38 +6,38 @@
 
 #include <ros/ros.h>
 
-#include <workshop_msgs/DetectionsStamped.h>
+#include <workshop_examples/DetectFaces.h>
+#include <workshop_msgs/Detections.h>
+
+bool detect(workshop_examples::DetectFaces::Request  &req,
+            workshop_examples::DetectFaces::Response &res
+            )
+{
+    ROS_INFO_STREAM("Request: detect_all = " << req.detect_all);
+
+    res.detections.type = workshop_msgs::Detections::FACE;
+
+    sensor_msgs::RegionOfInterest det;
+    det.x_offset = 100;
+    det.y_offset = 100;
+    det.width = 64;
+    det.height = 64;
+    res.detections.rects.push_back(det);
+
+    ROS_INFO_STREAM("...sending back response.");
+    return true;
+}
 
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "publisher");
 
     ros::NodeHandle n;
-    ros::Publisher pub = n.advertise<workshop_msgs::DetectionsStamped>("face_detections_out", 1000);
+    ros::ServiceServer service = n.advertiseService("detect_faces", detect);
 
-    ros::Rate loop_rate(10);
-    while( ros::ok() )
-    {
-        workshop_msgs::DetectionsStamped msg;
-        msg.header.stamp = ros::Time::now();
-        msg.header.frame_id = "";
-        msg.detections.type = workshop_msgs::Detections::FACE;
+    ROS_INFO("Service server ready.");
 
-        sensor_msgs::RegionOfInterest det;
-        det.x_offset = 100;
-        det.y_offset = 100;
-        det.width = 64;
-        det.height = 64;
-        msg.detections.rects.push_back(det);
-
-        ROS_INFO_ONCE( "Message sent" );
-
-        pub.publish(msg);
-
-        ros::spinOnce();
-        loop_rate.sleep();
-    }
+    ros::spin();
 
     return 0;
 }
-
