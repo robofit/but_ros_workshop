0bd574b9551f5c0b1ae54cb7c67eb443f1d93a24
but-spanel <spanel@fit.vutbr.cz>
2013-10-15 10:37:16 +0200

    Code review; Vita's samples and face detector has been added

--- workshop_examples/src/example_image_subscriber.cpp
+++ workshop_examples/src/example_image_subscriber.cpp
@@ -2,8 +2,10 @@
  *
  * Robo@FIT, ROS Workshop 2013
  *
- * Sample MS Kinect RGB image subsrciber
+ * Sample camera image subscriber
  */
+
+#include "opencv2/opencv.hpp"
 
 #include <ros/ros.h>
 
@@ -11,29 +13,33 @@
 #include <image_transport/image_transport.h>
 #include <sensor_msgs/image_encodings.h>
 
+
 void imageCallback(const sensor_msgs::ImageConstPtr& msg)
 {
     ROS_INFO_STREAM_ONCE("Image received.");
 
-    cv_bridge::CvImagePtr cv_ptr = cv_bridge::toCvCopy(msg);
-//    cv_bridge::CvImagePtr cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
-    cv::Mat depth = cv_ptr->image;
+//    cv_bridge::CvImagePtr cv_ptr = cv_bridge::toCvCopy(msg);
+    cv_bridge::CvImagePtr cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
+    cv::Mat img = cv_ptr->image;
 
     // ...
+
+    cv::imshow( "Image from Topic", img );
+    cv::waitKey(10);
 }
+
 
 int main( int argc, char** argv )
 {
-	ros::init(argc, argv, "kinect_image_subscriber");
+	ros::init(argc, argv, "example_image_subscriber");
 
 	ros::NodeHandle n;
     image_transport::ImageTransport it(n);
     image_transport::Subscriber sub = it.subscribe("image_in", 10, imageCallback);
 
-    ROS_INFO_STREAM("Kinect image subscriber initialized and listening...");
+    ROS_INFO_STREAM("Image subscriber initialized and listening...");
 
     ros::spin();
 
     return 1;
 }
-
